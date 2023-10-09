921f40fa30f62ad02383f2460df9f7147f44bae2
iannico322 <iannicocaulin@gmail.com>
2023-10-09 10:43:19 +0800

    feat: adding menus screen

---
+++ screens/API's/OpenAIImage.js
@@ -0,0 +1,172 @@
+
+import axios from 'axios'
+
+import CryptoJS from "crypto-js"
+
+import FormData from 'form-data';
+
+
+async function OpenAIImage({image}) {
+
+
+  const decryptText = (encryptedText)=> {
+    const bytes = CryptoJS.AES.decrypt(encryptedText, "itsy");
+    return bytes.toString(CryptoJS.enc.Utf8);
+  };
+
+ 
+
+  console.log(image)
+
+      const file =image;
+      const formData = new FormData();
+      formData.append('image', file);
+  
+      const options = {
+        method: 'POST',
+        url: 'https://microsoft-computer-vision3.p.rapidapi.com/describe',
+        params: {
+          language: 'en',
+          maxCandidates: '1',
+          'descriptionExclude[0]': 'Celebrities'
+        },
+        headers: {
+          'content-type': 'application/octet-stream',
+          'X-RapidAPI-Key': '096d7043d4msh2675ca63ad13d15p1e2935jsnacda8f927c83',
+          'X-RapidAPI-Host': 'microsoft-computer-vision3.p.rapidapi.com'
+        },
+        data: formData
+      };
+  
+     
+        const response = await axios(options);
+        console.log(response.data.description.captions[0].text)
+
+         const apiMessages =  { role: "user", content:` what is the food items on this paragraph: " ${response.data.description.captions[0].text}" 
+         expected output:
+          [
+            {"food_indentified": [
+                {
+                  "name":"food1",
+                  "quantity": "1pc"
+                },
+                {
+                  "name":"food2",
+                  "quantity": "1pc"
+                },
+                ...
+            ]
+            }
+        ]
+         `}
+
+
+          const apiRequestBody = {
+              "model": "gpt-3.5-turbo",
+              "messages": [
+                { 
+                  "role": "system",
+                   "content": `You are a professional advance AI that can extract Food from a paragrap. Your task is to generate a list of foods you identify on a sentences. This should be the following format: [{"food_indentified": [
+                    {
+                      "name":"food1",
+                      "quantity": "1pc"
+                     },
+                     {
+                      "name":"food2",
+                      "quantity": "1pc"
+                     },
+                     ...
+                   ]
+                  }]
+                  
+                  make sure to follow that format if you identify any Foods Items, if none just say  : No Food found!
+                  
+                  `,
+              
+                },
+                {
+                  "role":"user","content":` what is the food items on this paragraph: " a close up of a banana"`
+                },
+                { 
+                  "role": "assistant", "content": `[{"food_indentified": [
+                    {
+                      "name":"banana",
+                      "quantity": "1ps"
+                     }
+                   ]
+                  }]`
+              
+                },
+
+                {
+                  "role":"user","content":` what is the food items on this paragraph: "an apple and mango in a table"`
+                },
+                { 
+                  "role": "assistant", "content": `[{"food_indentified": [
+                    {
+                      "name":"apple",
+                      "quantity": "1ps"
+                     },
+                     {
+                      "name":"mango",
+                      "quantity": "1ps"
+                     }
+                   ]
+                  }]`
+                },
+
+                {
+                  "role":"user","content":` what is the food items on this paragraph: "a close up of a dog"`
+                },
+                { 
+                  "role": "assistant", "content": `No Food found!`
+                  
+              
+                },
+                {
+                  "role":"user","content":` what is the food items on this paragraph: "2 banana on the table"`
+                },
+                { 
+                  "role": "assistant", "content": `[{"food_indentified": [
+                    {
+                      "name":"shrimp",
+                      "quantity": "2ps"
+                     }
+                   ]
+                  }]`
+                  
+              
+                },
+                apiMessages
+              ]
+            }
+    
+
+            try {
+              const textresponse = await axios.post("https://api.openai.com/v1/chat/completions", apiRequestBody, {
+                headers: {
+                  Authorization: `Bearer ${ decryptText(localStorage.getItem('none')||"")}`,
+                  "Content-Type": "application/json",
+                }
+              });
+
+              console.log(textresponse.data.choices[0].message.content)
+              return textresponse.data.choices[0].message.content ;
+            } catch (error) {
+               return "Something went wrong";
+            }
+              
+           
+           
+      
+      
+      
+    
+
+
+
+        
+}
+
+export default OpenAIImage
+
---
+++ screens/API's/OpenAIText.js
@@ -0,0 +1,190 @@
+
+import axios from 'axios'
+
+import CryptoJS from "crypto-js"
+
+let cancelRequest; 
+
+async function OpenAIText({product}) {
+
+  const source = axios.CancelToken.source();
+
+  
+
+
+  const decryptText = (encryptedText)=> {
+    const bytes = CryptoJS.AES.decrypt(encryptedText, "itsy");
+    return bytes.toString(CryptoJS.enc.Utf8);
+  };
+
+ 
+
+  const bodyPrompt = `Generate at least 2 dishes in JSON format based on the following products, preferences, and language.
+  Products: [${product}]
+  Preferences: [${localStorage.getItem('SelectedPrefence')}]
+  Language [${localStorage.getItem('Languange')}]
+  
+  Output:
+  [
+      {
+          "name": "dish name",
+          "ingredients": ["ingredient1", "ingredient2", ...],
+          "cooking_steps": ["1. step1", "2. step2", ...]
+      },
+      ...
+  ]`
+
+   const apiMessages =  { role: "user", content: bodyPrompt}
+
+
+    const apiRequestBody = {
+        "model": "gpt-3.5-turbo",
+        "messages": [
+          { 
+            "role": "system", "content": `You are a professional chef with 10 years of experience. Your task is to generate a list of recipes.`,
+        
+          },
+          {
+            role:"user",content:`
+            Generate at least 2 dishes in JSON format based on the following products, preferences, and language:
+    
+            Products: [chicken,pieces dried bay leave, soy sauce,white vinegar,cloves garlic,water,cooking oil,sugar, salt,peppercorn]
+            Preferences: [Filipino Style]
+            Language: [english]
+            
+            Expected Output:
+            [
+              {
+                "name": "Dish Name",
+                "ingredients": ["ingredient1", "ingredient2", ...],
+                "cooking_steps": ["1. step1", "2. step2", ...]
+              },
+    
+        
+            `
+          },
+          { 
+            "role": "assistant", "content": `[
+              {
+                  "name": "Chicken Adob",
+                  "ingredients": ["2 lbs chicken", "3 pieces dried bay leaves","6 tablespoons soy sauce","4 tablespoons white vinega","5 cloves garlic","1 1/2 cups water","3 tablespoons cooking oil","1 teaspoon sugar","1/4 teaspoon salt","1 teaspoon whole peppercorn"],
+                  "cooking_steps": ["1. Combine chicken, soy sauce, and garlic in a large bowl. Mix well. Marinate the chicken for at least 1 hour. Note: the longer the time, the better 2 lbs chicken, 6 tablespoons soy sauce", "2. Heat a cooking pot. Pour cooking oil. 3 tablespoons cooking oil", "3. When the oil is hot enough, pan-fry the marinated chicken for 2 minutes per side."," 4. Pour-in the remaining marinade, including garlic. Add water. Bring to a boil 1 1/2 cups water", "5. Add dried bay leaves and whole peppercorn. Simmer for 30 minutes or until the chicken gets tender 3 pieces dried bay leaves, 1 teaspoon whole peppercorn","6. Add vinegar. Stir and cook for 10 minutes. 4 tablespoons white vinegar","Put-in the sugar, and salt. Stir and turn the heat off.Serve hot. Share and Enjoy! 1 teaspoon sugar, 1/4 teaspoon salt"]
+              },
+              ...
+          ]
+            `,
+        
+          },
+          {
+        
+            role:"user",content:`
+            Generate at least 2 dishes in JSON format based on the following products, preferences, and language:
+    
+            Products: [chicken,pieces dried bay leave, soy sauce,white vinegar,cloves garlic,water,cooking oil,sugar, salt,peppercorn]
+            Preferences: [Filipino Style]
+            Language: [Bisaya]
+            
+            Expected Output:
+            [
+              {
+                "name": "Dish Name",
+                "ingredients": ["ingredient1", "ingredient2", ...],
+                "cooking_steps": ["1. step1", "2. step2", ...]
+              },
+    
+        
+            `
+  
+        },
+        { 
+          "role": "assistant", "content": `[
+            {
+                "name": "Chicken Adob",
+                "ingredients": ["2 lbs nga manok", "3 ka piraso nga uga nga dahon sa bay","6 ka kutsarang toyo","4 ka kutsarang puti nga suka","5 ka cloves nga ahos", "1 1/2 ka tasang tubig","3 kutsara nga mantika sa pagluto ","1 ka kutsarita nga asukar", "1/4 ka kutsarita nga asin", "1 ka kutsarita nga tibuok peppercorn"],
+                "cooking_steps": [1. Isagol ang manok, toyo, ug ahos sa usa ka dako nga panaksan. Sagola og maayo. I-marinate ang manok sulod sa labing menos 1 ka oras. Pahinumdom: mas taas ang oras, mas maayo 2 lbs nga manok, 6 ka kutsara nga toyo", "2. Pag-init og kaldero. Ibubo ang lana sa pagluto. 3 ka kutsara nga lana sa pagluto", "3. Kung init na ang mantika, i-pan-fry ang gi-marinate nga manok sulod sa 2 ka minuto matag kilid."," 4. Ibubo ang nahabilin nga marinade, lakip ang ahos. Idugang ang tubig. Dad-a sa usa ka hubag 1 1/2 tasa tubig", "5. Idugang ang uga nga dahon sa bay ug tibuok peppercorn. Lutoa sulod sa 30 minutos o hangtud nga ang manok mohumok 3 ka piraso nga uga nga dahon sa bay, 1 ka kutsarita nga tibuok peppercorn","6. Idugang ang suka. Pagpalihok ug pagluto sulod sa 10 minutos. 4 tablespoons puti nga suka","Ibutang-sa asukar, ug asin. Pagpalihok ug palonga ang kainit.Isilbi nga init. Ipakigbahin ug Enjoy! 1 ka kutsarita nga asukar, 1/4 ka kutsarita nga asin"]
+            },
+            ...
+        ]
+          `,
+      
+        },
+        {
+        
+          role:"user",content:`
+          Generate at least 2 dishes in JSON format based on the following products, preferences, and language:
+  
+          Products: [chicken,pieces dried bay leave, soy sauce,white vinegar,cloves garlic,water,cooking oil,sugar, salt,peppercorn]
+          Preferences: [Filipino Style]
+          Language: [UwU]
+          
+          Expected Output:
+          [
+            {
+              "name": "Dish Name",
+              "ingredients": ["ingredient1", "ingredient2", ...],
+              "cooking_steps": ["1. step1", "2. step2", ...]
+            },
+  
+      
+          `
+
+      },
+      { 
+        "role": "assistant", "content": `[
+          {
+              "name": "Chicken Adobo >_<",
+              "ingredients": ["2 lbs chicken", "3 pieces dried bay leaves","6 tablespoons soy sauce","4 tablespoons white vinega","5 cloves garlic","1 1/2 cups water","3 tablespoons cooking oil","1 teaspoon sugar","1/4 teaspoon salt","1 teaspoon whole peppercorn"],
+              "cooking_steps": [
+                "1. OwO Combine chicken, soy sauce, and garlic in a warge bowl. Mix well. Mawinate the chicken fow at weast 1 hour. Notice: the wongew the time, the bettew! ^w^ 2 lbs chicken, 6 tabwespoons soy sauce",
+                "2. Heat a cooking pot. Pour cooking oiw. 3 tabwespoons cooking oiw OwO",
+                "3. When the oiw is hot enough, pan-fwy the mawinated chicken fow 2 minutes pew side. UwU",
+                "4. Pour-in the wemaining mawinade, incwuding garlic. Add watew. Bwing to a boiw. 1 1/2 cups watew",
+                "5. Add dwied bay weaves and whole peppewcorn. Simmew fow 30 minutes ow untiw the chicken gets tendew. 3 pieces dwied bay weaves, 1 teaspoon whole peppewcorn",
+                "6. Add vinegar. Stiw and cook fow 10 minutes. 4 tabwespoons white vinegar",
+                "Put-in the sugaw and sawt. Stiw and tuwn the heat off. Sewve hot. Shawe and Enjoy! ^_^ 1 teaspoon sugaw, 1/4 teaspoon sawt"
+                ]
+          },
+          ...
+      ]
+        `,
+    
+      },
+          apiMessages
+          
+        ]
+      }
+
+      
+      cancelRequest = () => {
+        source.cancel('Operation canceled by the user.');
+       
+        
+      }
+
+     try {
+    const response = axios.post("https://api.openai.com/v1/chat/completions", apiRequestBody, {
+      cancelToken: source.token,
+      headers: {
+        Authorization: `Bearer ${ decryptText(localStorage.getItem('none')||"")}`,
+        "Content-Type": "application/json",
+      }
+    });
+    return JSON.parse((await response).data.choices[0].message.content);
+  } catch (e) {
+    return [];
+  }
+
+  
+
+
+ 
+
+
+
+
+        
+}
+
+export default OpenAIText
+
+export { cancelRequest };
